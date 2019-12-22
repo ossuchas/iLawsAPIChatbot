70bbdf804b5dc50cab2f3151970590353306e800
ossuchas <suchat.s14102526@gmail.com>
2019-12-23 05:11:46 +0700

    make it better

--- app.py
+++ app.py
@@ -1,12 +1,28 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask_restful import Api
+from dotenv import load_dotenv
+from flask_cors import CORS
+from marshmallow import ValidationError
+
+from db import db
+from ma import ma
 
 app = Flask(__name__)
+
+api = Api(app, prefix="/api/v1")
+CORS(app, resources=r"/api/*", allow_headers="Content-Type")
+
+load_dotenv(".env", verbose=True)
+app.config.from_object("config")
+app.config.from_envvar("APPLICATION_SETTING")
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return 'iLaw Chatbot Hello World 1.0.0..!!'
 
 
 if __name__ == '__main__':
-    app.run()
+    db.init_app(app)
+    ma.init_app(app)
+    app.run(host="0.0.0.0", port=5000, debug=True)
